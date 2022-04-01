a1b377611254bc04afe2ab41cd532a14d45167b4
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-01 11:08:57 +0200

    ProxyCache : Initialized Cache, Service and Data Model

---
+++ ProxyCache/JCDecauxModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProxyCache
+{
+    [DataContract]
+    public class JCDecauxModel
+    {
+        
+    }
+
+    [DataContract]
+    public class StationModel
+    {
+        
+    }
+
+    [DataContract]
+    public class PositionModel
+    {
+       
+    }
+}
---
+++ ProxyCache/JCDecauxService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+//using Proxy.Cache;
+//using Proxy.Models;
+using System.Collections.Generic;
+
+
+namespace ProxyCache
+{
+    public class JCDecaux : IJCDecauxService
+    {
+    }
+
+}
