ab571f8eb705f9cfa11f48ba14ccbfb44ab1803a
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-01 11:48:25 +0200

    Implement JCDecaux constructor with API stations call

--- ProxyCache/JCDecauxModel.cs
+++ ProxyCache/JCDecauxModel.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Runtime.Serialization;
-
+using System.Text.Json;
+using System.Threading.Tasks;
 
 
 namespace ProxyCache
@@ -11,7 +12,7 @@ namespace ProxyCache
     {
         private static readonly string API_key = "c8e8deaba5b4c9af98626d23aad7bff03fe3b1b9";
         private static readonly string url = "https://api.jcdecaux.com/vls/v2/";
-        private static readonly string data = "stations";
+        private static readonly string path = "stations";
 
         private string request;
         private static readonly HttpClient client = new HttpClient();
@@ -22,6 +23,28 @@ namespace ProxyCache
         public JCDecauxModel()
         {
 
+        }
+
+        public JCDecauxModel(Dictionary<string, string> infos)
+        {
+            request = url + path + "/" + infos["station_number"] + "?contract=" + infos["contract_name"] + "&apiKey=" + API_key;
+            station = CallREST(request).Result;
+        }
+
+        private static async Task<StationModel> CallREST(string request)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(request);
+                response.EnsureSuccessStatusCode();
+                string returnedBody = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<StationModel>(returnedBody);
+            }
+            catch (HttpRequestException)
+            {
+                return new StationModel();
+            }
         }
 
 
