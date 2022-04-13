57562248d9a986975a40ddcf5578ac38091e2ffd
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-13 11:07:42 +0200

    implementation of JCDecaux Service for linking Cache and Item

--- ProxyCache/JCDecauxService.cs
+++ ProxyCache/JCDecauxService.cs
@@ -12,6 +12,25 @@ namespace ProxyCache
 {
     public class JCDecaux : IJCDecauxService
     {
+        private static string endpoint = "station";
+
+        private static Cache<JCDecauxItem> cache = new Cache<JCDecauxItem>();
+
+        public JCDecauxItem GetStationDefault(string city, string stationNumber)
+        {
+            //Default expiration time is now+60 seconds as in the subject.
+            return GetStation(city, stationNumber, 60);
+        }
+
+        public JCDecauxItem GetStation(string city, string stationNumber, double duration)
+        {
+            Dictionary<string, string> infos = new Dictionary<string, string>
+            {
+                { "contract_name", city },
+                { "station_number", stationNumber }
+            };
+            return cache.Get(endpoint + "?contract=" + city + "&number=" + stationNumber, duration/*, infos*/);
+        }
     }
 
 }
