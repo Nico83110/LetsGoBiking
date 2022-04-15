a33714b4bc83fa9d199a3f4f44f386a28e8c4d1b
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-15 20:40:53 +0200

    Made ProxyCache as self-hosted

---
+++ ProxyCache/StartPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyCache
+{
+    internal class StartPoint
+    {
+        // Host the service within this EXE console application.
+        public static void Main()
+        {
+            // Create a ServiceHost for the CalculatorService type.
+            using (
+                ServiceHost serviceHost =
+                new ServiceHost(typeof(JCDecauxService)))
+            {
+                // Open the ServiceHost to create listeners
+                // and start listening for messages.
+                serviceHost.Open();
+
+                // The service can now be accessed.
+                Console.WriteLine("Le service RoutingServer est prêt à l'adresse : {0}", serviceHost.BaseAddresses[0]);
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
