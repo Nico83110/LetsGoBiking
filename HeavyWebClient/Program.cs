5fd5cfed26b11f76f6350be6d6cabd48dc9be562
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-16 23:18:44 +0200

    implemented call from HeavyClient to RoutingServer contract GetAllStations

--- HeavyWebClient/Program.cs
+++ HeavyWebClient/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace HeavyWebClient
+﻿using System;
+using System.ServiceModel;
+using RoutingServer;
+using ProxyCache;
+
+namespace HeavyWebClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello from the Heavy Client !");
+
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress endpoint = new EndpointAddress("http://localhost:8733/RoutingServer/Service1/");
+            ChannelFactory<IRoutingServerService> myChannelFactory = new ChannelFactory<IRoutingServerService>(binding, endpoint);
+            IRoutingServerService wcfClient = myChannelFactory.CreateChannel();
+
+            List<StationModel> stations = wcfClient.GetAllStations();
+            Console.WriteLine("Voici le contenu de la liste de toutes les stations : \n");
+            Console.WriteLine(stations);
         }
     }
 }
