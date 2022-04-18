bd6e659617d730dbf3b3a2fff52660177c719457
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-18 23:02:10 +0200

    created class for OpenStreetMap related calls

---
+++ RoutingServer/ExternalCalls/OpenStreetMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ProxyCache;
+
+namespace RoutingServer.ExternalCalls
+{
+    internal class OpenStreetMap
+    {
+        private static HttpClient client = new HttpClient();
+
+        public OpenStreetMap()
+        {
+        }
+        public Position GetPositionOfAddress(string address)
+        {
+            return null;
+        }
+
+    }
+
+    public class Place
+    {
+        public float importance { get; set; }
+        public string lat { get; set; }
+
+        public string lon { get; set; }
+
+    }
+}
