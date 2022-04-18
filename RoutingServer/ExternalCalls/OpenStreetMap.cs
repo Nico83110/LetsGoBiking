3301eb6dcfe21538977d1ee008267bd31f3e6400
Nico83110 <nicolas.perrin83110@gmail.com>
2022-04-18 23:45:21 +0200

    implementation of getting position from address using Nominatim

--- RoutingServer/ExternalCalls/OpenStreetMap.cs
+++ RoutingServer/ExternalCalls/OpenStreetMap.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using ProxyCache;
+using System.Runtime.Serialization;
+using System.Text.Json;
 
 namespace RoutingServer.ExternalCalls
 {
@@ -17,15 +19,59 @@ namespace RoutingServer.ExternalCalls
         }
         public Position GetPositionOfAddress(string address)
         {
-            return null;
+            List<Place> places = GetPlacesNearAdress(address).Result;
+            float score = 0;
+            Place nearestPlace = null;
+
+            foreach(Place place in places)
+            {
+                if(place.importance > score)
+                {
+                    score = place.importance;
+                    nearestPlace = place;
+                }
+            }
+
+            Position result = new Position
+            {
+                latitude = float.Parse(nearestPlace.lat),
+                longitude = float.Parse(nearestPlace.lon)
+            };
+
+            return result;
         }
+        public async Task<List<Place>> GetPlacesNearAdress(string address)
+        {
+            string req = "https://nominatim.openstreetmap.org/search?format=json&q=" + address;
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(req);
+                response.EnsureSuccessStatusCode();
+                string body = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<List<Place>>(body);
+            }
+            catch (HttpRequestException)
+            {
+                return null;
+            }
+        }
+
 
     }
 
+    [DataContract]
     public class Place
     {
+        public Place()
+        {
+        }
+
+        [DataMember]
         public float importance { get; set; }
+        [DataMember]
         public string lat { get; set; }
+        [DataMember]
 
         public string lon { get; set; }
 
